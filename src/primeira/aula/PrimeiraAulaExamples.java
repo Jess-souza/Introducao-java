5a02af2a1279ec10404d5d34ff9c729c9ecee3a0
Jessica <jsouzasouza120@gmail.com>
2022-09-19 21:41:08 -0300

    first commit

---
+++ src/primeira/aula/PrimeiraAulaExamples.java
@@ -0,0 +1,14 @@
+package primeira.aula;
+
+public class PrimeiraAulaExamples {
+    public static void main(String[] args) {
+        int numero = 632687093;
+        float peso = 32.98f;
+        double altura = 1.64;
+        boolean flag = false;
+        char letra = 'N';
+        String nome = "Jéssica";
+
+        System.out.println(letra);
+    }
+}
