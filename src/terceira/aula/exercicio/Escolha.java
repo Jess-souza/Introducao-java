ef12c5000b4d49cb17a19c063975504ee27e6148
Jessica <jsouzasouza120@gmail.com>
2022-09-23 21:46:09 -0300

    Realização do exercício

---
+++ src/terceira/aula/exercicio/Escolha.java
@@ -0,0 +1,43 @@
+package terceira.aula.exercicio;
+
+import java.util.Scanner;
+
+public class Escolha {
+    public static void main(String[] args) {
+        System.out.println("Escolha uma opção de pagamento: ");
+        System.out.println("1- Dinheiro");
+        System.out.println("2- Cartão de crédito");
+        System.out.println("3- Cartão de débito");
+        System.out.println("4- A prazo");
+        System.out.println("5- Pix");
+
+        Scanner sc = new Scanner(System.in);
+        int escolha = sc.nextInt();
+        sc.close();
+
+        FormaDePagamento es = FormaDePagamento.values()[--escolha];
+
+        switch (es){
+            case DINHEIRO :
+                System.out.println("A forma de pagamento escolhida foi dinheiro");
+                break;
+            case CARTAO_DE_CREDITO:
+                System.out.println("A forma de pagamento escolhida foi cartão de crédito");
+                break;
+            case CARTAO_DEBITO:
+                System.out.println("A forma de pagamento escolhida foi cartão de débito");
+                break;
+            case A_PRAZO:
+                System.out.println("A forma de pagamento escolhida foi a prazo");
+                break;
+            case PIX:
+                System.out.println("A forma de pagamento escolhida foi pix");
+
+
+        }
+
+
+
+
+    }
+}
---
+++ src/terceira/aula/exercicio/FormaDePagamento.java
@@ -0,0 +1,9 @@
+package terceira.aula.exercicio;
+
+public enum FormaDePagamento {
+    DINHEIRO,
+    CARTAO_DE_CREDITO,
+    CARTAO_DEBITO,
+    A_PRAZO,
+    PIX
+}
