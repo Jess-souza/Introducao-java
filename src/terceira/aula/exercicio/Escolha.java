61a34a7672fe5a6934db8bb2be560f357e063fe5
Jessica <jsouzasouza120@gmail.com>
2022-09-23 22:11:13 -0300

    Atualização

--- src/terceira/aula/exercicio/Escolha.java
+++ src/terceira/aula/exercicio/Escolha.java
@@ -11,14 +11,15 @@ public class Escolha {
         System.out.println("4- A prazo");
         System.out.println("5- Pix");
 
+
         Scanner sc = new Scanner(System.in);
         int escolha = sc.nextInt();
         sc.close();
 
         FormaDePagamento es = FormaDePagamento.values()[--escolha];
 
-        switch (es){
-            case DINHEIRO :
+        switch (es) {
+            case DINHEIRO:
                 System.out.println("A forma de pagamento escolhida foi dinheiro");
                 break;
             case CARTAO_DE_CREDITO:
@@ -32,12 +33,7 @@ public class Escolha {
                 break;
             case PIX:
                 System.out.println("A forma de pagamento escolhida foi pix");
-
-
+                break;
         }
-
-
-
-
     }
 }
